45ae8a9adffcb88f735511e56f692b0abda614da
zhengjialu8507 <zhengjialu8507@gmail.com>
2024-09-20 11:18:00 +0800

    适配另一种简单的数据结构展示

--- src/serviceApi/getLineageDataApi.js
+++ src/serviceApi/getLineageDataApi.js
@@ -1,8 +1,15 @@
 import axios from "axios"
 import lineageData from "../mock/getLineage.json"
+import flinkLineageData from "../mock/_getLineage.json"
 
 export const getLineageDataByFQN = (params) => {
     return new Promise((resolve, reject) => {
         resolve(lineageData)
+    })
+}
+
+export const getLineageDataByFlink = (params) => {
+    return new Promise((resolve, reject) => {
+        resolve(flinkLineageData)
     })
 }
