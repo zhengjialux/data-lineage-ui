7007c867d46df1caafdcb345fca5d063570535c7
zhengjialu8507 <zhengjialu8507@gmail.com>
2024-11-27 15:23:34 +0800

    update

--- src/components/lineage/LineageProvider.jsx
+++ src/components/lineage/LineageProvider.jsx
@@ -71,7 +71,7 @@ const LineageApp = ({ children }) => {
   useEffect(() => {
     // getLineageDataByFQN(
     //   // {
-    //   //   查询主表名 fqn: TEXT-MySQL.data_test.cascade_connection_test.cdc_table
+    //   //   查询主表名 fqn: TEST-MySQL.data_test.cascade_connection_test.cdc_table
     //   //   血缘类型 type: table
     //   //   向上查询血缘层级 upstreamDepth: 3
     //   //   向下查询血缘层级 downstreamDepth: 3
